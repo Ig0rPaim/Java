3dc5bc4f532ddc27e1e6ab0ab5eaaedbb105acf0
Igor Paim de Oliveira <igorpaimdeoliveira@gmail.com>
2024-04-10 15:57:07 -0300

    authorization

--- springLearning/src/main/java/com/crud/springLearning/controllers/UserController.java
+++ springLearning/src/main/java/com/crud/springLearning/controllers/UserController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,6 +36,7 @@ public class UserController {
 
     @Transactional
     @DeleteMapping("/{id}")
+    @Secured("user_admin")
     public ResponseEntity<UserDTO>  deleteById(@PathVariable Long id){
         return userService.deleteById(id);
     }
